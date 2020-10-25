a2dc13daa347fde639b7c7ba66e322f19cef156c
CHENGpeppermint <1309231080@qq.com>
2020-10-25 20:22:21 +0800

    3

---
+++ precache-manifest.a0a491d2debf97347fe96393e5b88c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b452d453a57a04e7db59357d1b5311f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7c8ff7d4a3d920481b",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "0d7c8ff7d4a3d920481b",
+    "url": "/static/js/2.4ba57cda.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4ba57cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ab2e8836468134654f",
+    "url": "/static/js/main.57cbc21a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "9e821e1dc72ee02da29b923927580df9",
+    "url": "/static/media/banner1.9e821e1d.png"
+  }
+]);
